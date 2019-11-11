f39deaf4a6000c58876707dd674cc1e6681103f2
Patrick <patrick.dooko@adncorp.com>
2019-11-11 14:35:33 +0000

    create public folder for assets

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require("path");
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT || 4000, function() {
   console.log("Node app is working!");
