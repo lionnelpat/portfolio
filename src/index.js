d0e7ef87df23742eb295ae861bea2c2ba99a5d2d
LionnelPat <patrick.dooko@adncorp.com>
2019-11-12 18:08:01 +0000

    update package.json

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ var app = express();
 var path = require("path");
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.js"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "How do I deploy my code to Heroku using GitLab CI/CD?"
+  });
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(process.env.PORT || 4000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Node app is working!");
 });
